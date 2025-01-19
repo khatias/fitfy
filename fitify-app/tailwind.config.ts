7a84ffe1eedb6fde59a7e3d51eff964bb553fb7f
khatia <130936157+khatias@users.noreply.github.com>
2025-01-18 23:52:22 -0500

    feat: add FontAwesome, Heroicons, and custom colors
    - Added FontAwesome and Heroicons for icons
    - Configured custom colors in Tailwind

--- fitify-app/tailwind.config.ts
+++ fitify-app/tailwind.config.ts
@@ -13,6 +13,12 @@ export default {
         georgian: ["var(--font-noto-sans-georgian)", "sans-serif"],
       },
       colors: {
+        customDark: '#12131a',
+        customYellow: '#d79922',
+        customAmber: '#efb11d',
+        customRed: '#e43d12',
+        customBlueGray: '#2e3944',
+        customGray: '#f5f5f5', 
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
