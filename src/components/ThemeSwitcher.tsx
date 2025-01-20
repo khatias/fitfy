52ab8ad9e750ece4fc5f45ff3943851d32e7ee78
khatia <130936157+khatias@users.noreply.github.com>
2025-01-19 19:21:50 -0500

    feat: add theme switcher with sun and moon icons
    - Implemented a theme switcher toggle for light/dark modes
    - Added smooth animations with sun and moon icons transitioning based on theme

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -2,15 +2,10 @@
 import React from "react";
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
-
-const themes = [
-  { value: "light", label: "Light" },
-  { value: "dark", label: "Dark" },
-];
+import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
-  console.log(theme);
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -22,18 +17,29 @@ const ThemeSwitcher = () => {
 
   return (
     <div>
-      <div>
-        {themes.map(({ value, label }) => (
-          <button
-            key={value}
-            onClick={() => {
-              setTheme(value);
-            }}
-          >
-            {label}
-          </button>
-        ))}
-      </div>
+      <label className="relative inline-flex items-center cursor-pointer">
+        <input
+          type="checkbox"
+          checked={theme === "dark"}
+          onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+          className="sr-only"
+        />
+        <div
+          className={`w-12 h-7 flex items-center justify- bg-customGray  dark:bg-gray-700 rounded-full p-1 transition-all duration-300 ease-in-out`}
+        >
+          <SunIcon
+            className={`w-6 h-6 text-gray-400 transform transition-all duration-500 ease-in-out ${
+              theme === "dark" ? "rotate-180 opacity-0" : "rotate-0 opacity-100"
+            }`}
+          />
+
+          <MoonIcon
+            className={`w-6 h-6 text-gray-400 transform transition-all duration-500 ease-in-out ${
+              theme === "dark" ? "rotate-0 opacity-100" : "rotate-180 opacity-0"
+            }`}
+          />
+        </div>
+      </label>
     </div>
   );
 };
