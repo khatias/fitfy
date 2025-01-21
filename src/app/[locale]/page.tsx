da0a52e68b79c67a3ff65e24b7d5a4d3dbd3e3c1
khatia <130936157+khatias@users.noreply.github.com>
2025-01-21 14:45:49 -0500

    feat: Implement language switcher UI and locale-based font change
    - Added  language switcher with dropdown functionality.
    - Implemented locale-based font adjustments to dynamically adapt text styling based on the selected language.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,13 @@
 import { useTranslations } from "next-intl";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
-
+import LanguageSwitcher from "@/components/LanguageSwitcher";
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
     <div>
       <h1>{t("title")}</h1>
       <ThemeSwitcher />
+      <LanguageSwitcher />
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-import { Montserrat, Noto_Sans_Georgian } from "next/font/google";
-import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
-const notoSansGeorgian = Noto_Sans_Georgian({
-  variable: "--font-noto-sans-georgian",
-  subsets: ["georgian"],
-});
-const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Fitify App",
@@ -19,20 +10,10 @@ export const metadata: Metadata = {
     "Fitify is an online store offering a wide range of stylish clothing. Shop the latest trends and find your perfect fit!",
 };
 
-export default function RootLayout({
-  children,
-}: {
+interface Props {
   children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body
-        className={` ${montserrat.variable} ${notoSansGeorgian.variable} antialiased bg-white dark:bg-black text-black dark:text-white transition-colors duration-200 ease-in`}
-      >
-        <ThemeProvider defaultTheme="system" enableSystem attribute="class">
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+}
+
+export default function RootLayout({ children }: Props) {
+  return children;
 }
