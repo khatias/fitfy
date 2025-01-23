777d803e2520996c50dd57c180999fdfe94b809d
khatia <130936157+khatias@users.noreply.github.com>
2025-01-23 10:30:34 -0500

    feat: added reset password UI with send email form

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -7,42 +7,65 @@ import { handleGithubLogin } from "@/utils/auth/handleGithubLogin";
 
 const LoginPage = () => {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [resetPassword, setResetPassword] = useState<boolean>(false);
 
   return (
     <div>
-      <form
-        className="max-w-96 flex flex-col "
-        onSubmit={(e) => handleAuthSubmit(e, "login", setErrorMessage)}
-      >
-        <Input
-          data-cy="login-email-input"
-          label="Email Address"
-          type="email"
-          placeholder="Enter your email"
-          name="email"
-          required={true}
-        />
-
-        <Input
-          data-cy="login-password-input"
-          label="Password"
-          type="password"
-          placeholder="Enter your password"
-          name="password"
-          required={true}
-        />
-        {errorMessage && (
-          <div
-            data-cy="login-error-message"
-            className="text-center text-red-500 mt-4"
+      {!resetPassword && (
+        <>
+          {" "}
+          <form
+            className="max-w-96 flex flex-col "
+            onSubmit={(e) => handleAuthSubmit(e, "login", setErrorMessage)}
           >
-            <strong>{errorMessage}</strong>
-          </div>
-        )}
-        <SubmitButton text="Log In" />
+            <Input
+              data-cy="login-email-input"
+              label="Email Address"
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              required={true}
+            />
 
-        <button onClick={handleGithubLogin}>sing in with github</button>
-      </form>
+            <Input
+              data-cy="login-password-input"
+              label="Password"
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              required={true}
+            />
+            {errorMessage && (
+              <div
+                data-cy="login-error-message"
+                className="text-center text-red-500 mt-4"
+              >
+                <strong>{errorMessage}</strong>
+              </div>
+            )}
+            <button onClick={() => setResetPassword(!resetPassword)}>
+              resset my password{" "}
+            </button>
+            <SubmitButton text="Log In" />
+
+            <button onClick={handleGithubLogin}>sing in with github</button>
+          </form>
+        </>
+      )}
+      {resetPassword && (
+        <div>
+          <form action="">
+            <Input
+              label="Email Address"
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              required={true}
+            />
+            <button>send email</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
