3c328d87a2cb5cb06b408ac19662361401e00c11
khatia <130936157+khatias@users.noreply.github.com>
2025-01-25 01:14:51 -0500

    feat(profile): Add logic to fetch, update, and modify profile information
    - Implemented API call to fetch profile data from the server
    - Added functionality to allow users to edit and save profile information
    - Updated state management to handle changes in profile data
    - Styled the profile editor for better user experience

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const withNextIntl = createNextIntlPlugin();
- 
+import createNextIntlPlugin from "next-intl/plugin";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "unfdxueausqwhlaohxcp.supabase.co",
+      "lh3.googleusercontent.com",
+    ],
+  },
+};
+
+const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
