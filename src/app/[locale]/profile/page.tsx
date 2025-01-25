fc29ff9f9073452da4de8b3a0318f8869c950782
khatia <130936157+khatias@users.noreply.github.com>
2025-01-25 10:51:35 -0500

    feat: create reusable getUserProfile function
    - Refactored the getUserProfile logic into a reusable function.
    - Simplifies code reuse and improves maintainability.
    - Can be integrated across multiple components or modules.

--- src/app/[locale]/profile/page.tsx
+++ src/app/[locale]/profile/page.tsx
@@ -1,31 +1,8 @@
-import { createClient } from "@/utils/supabase/server";
 import ProfileEditor from "@/components/profile/ProfileEditor";
-import { Profile } from "@/types/profile";
 
+import { getUserProfile } from "@/utils/profile/getUserProfile";
 export default async function ProfileClient() {
-  const supabase = await createClient();
-  const { data: userResponse } = await supabase.auth.getUser();
-  const user = userResponse?.user;
-  console.log(user);
-  const profile: Profile = {
-    firstName: user?.user_metadata?.firstName || "",
-    lastName: user?.user_metadata?.lastName || "",
-    userName:
-      user?.user_metadata?.userName || user?.user_metadata?.user_name || "",
-    location: user?.user_metadata?.location || "",
-    phoneNumber: user?.user_metadata?.phone_number || "",
-    birthday: user?.user_metadata?.birthday || "",
-    email: user?.email || "",
-    avatar_url: user?.user_metadata?.avatar_url || null,
-    lastSignInAt: user?.last_sign_in_at || null,
-    address: {
-      street: user?.user_metadata?.address?.street || "",
-      city: user?.user_metadata?.address?.city || "",
-      state: user?.user_metadata?.address?.state || "",
-      postalCode: user?.user_metadata?.address?.postalCode || "",
-      country: user?.user_metadata?.address?.country || "",
-    },
-  };
+  const profile = await getUserProfile();
 
   return (
     <div className="container mx-auto px-4 dark:bg-gray-900 dark:text-white">
