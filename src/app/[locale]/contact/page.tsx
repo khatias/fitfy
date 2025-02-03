6fbf4c427853e2793f4953ff20b2a3d8135a9b0e
khatia <130936157+khatias@users.noreply.github.com>
2025-02-03 00:24:40 -0500

    feat: add contact form

---
+++ src/app/[locale]/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ContactForm from "@/components/contact/ContactForm";
+export default function page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-white">
+      <ContactForm />
+    </div>
+  );
+}
